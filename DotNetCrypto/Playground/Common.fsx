57ad925cd89df6501ca8a0f0b4a7d91b6003d324
pihai <philipp-haider@aon.at>
2016-01-23 17:27:52 +0100

    - Added common encode and decode functions - Added AES

---
+++ DotNetCrypto/Playground/Common.fsx
@@ -0,0 +1,9 @@
+﻿module Common
+
+open System.Text
+
+let encode (text: string) = Encoding.UTF8.GetBytes text
+let decode (bytes: byte array) = Encoding.UTF8.GetString bytes
+
+let plainText = "This is a highly confidential message"
+let plainBytes = encode plainText
---
+++ DotNetCrypto/Playground/Symmetric.fsx
@@ -0,0 +1,29 @@
+﻿#load "Common.fsx"
+
+open Common
+
+open System.Security.Cryptography
+open System.IO
+
+let encrypt plain =
+  use aes = Aes.Create()
+  // shortcut for: use aes = new AesCryptoServiceProvider()
+
+  use ms = new MemoryStream()
+  use cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write)
+
+  cs.Write(plain, 0, Array.length plain)
+  cs.FlushFinalBlock()
+
+  ms.ToArray(), aes.Key, aes.IV
+
+let decrypt cipher key iv =
+  use aes = Aes.Create()
+  use ms = new MemoryStream(cipher, false)
+  use cs = new CryptoStream(ms, aes.CreateDecryptor(key, iv), CryptoStreamMode.Read)
+  use br = new BinaryReader(cs)
+  use sr = new StreamReader(cs)
+  sr.ReadToEnd()
+
+let cipher, key, iv = encrypt plainBytes
+let decrypted = decrypt cipher key iv
