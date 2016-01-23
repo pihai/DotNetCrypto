ac79b3471b1ad60f4e548ba3bd3c820cb13c9004
pihai <philipp-haider@aon.at>
2016-01-23 17:59:56 +0100

    - Added RSA

---
+++ DotNetCrypto/Playground/Asymmetric.fsx
@@ -0,0 +1,25 @@
+﻿#load "Common.fsx"
+
+open Common
+
+open System.Security.Cryptography
+open System.IO
+
+let encrypt plain rsaParams =
+  use rsa = new RSACryptoServiceProvider()
+  rsa.ImportParameters rsaParams
+  rsa.Encrypt(plain, true)
+
+let decrypt cipher rsaParams =
+  use rsa = new RSACryptoServiceProvider()
+  rsa.ImportParameters rsaParams
+  rsa.Decrypt(cipher, true)
+
+let rsa = new RSACryptoServiceProvider()
+
+// encrypt with the public key
+let cipherBytes = encrypt plainBytes (rsa.ExportParameters(false))
+
+// decrypt with the private key
+let decryptedBytes = decrypt cipherBytes (rsa.ExportParameters(true))
+let decryptedText = decode decryptedBytes
