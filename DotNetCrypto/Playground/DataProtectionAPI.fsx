937a85c4fe55f432658f8d28baffa5b7ce171f23
pihai <philipp-haider@aon.at>
2016-01-24 10:11:26 +0100

    - Added Data Protection API

---
+++ DotNetCrypto/Playground/DataProtectionAPI.fsx
@@ -0,0 +1,10 @@
+﻿#r "System.Security"
+#load "Common.fsx"
+
+open System.Security.Cryptography
+open Common
+
+// The optional entropy is comparable with an initialization vector to add some randomness
+let cipher = ProtectedData.Protect(plainBytes, Array.empty<byte>, DataProtectionScope.CurrentUser)
+let decryptedBytes = ProtectedData.Unprotect(cipher, Array.empty<byte>, DataProtectionScope.CurrentUser)
+let decryptedTest = decode decryptedBytes
