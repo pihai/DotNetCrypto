a51e938daa711fb23645de4af3923a8af9db5005
pihai <philipp-haider@aon.at>
2016-01-26 20:26:31 +0100

    - First version of the floating license system - It is working but totally without security

---
+++ DotNetCrypto/Client/Program.fs
@@ -0,0 +1,52 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open System.Net.Http
+open System.Net
+
+let port = 8083
+
+let requestLicenseToken ipAddress = async {
+  use client = new HttpClient()
+  let! response = client.GetAsync(sprintf "http://%s:%d/GetLicense" ipAddress port) |> Async.AwaitTask
+  if response.IsSuccessStatusCode then
+    let! token = response.Content.ReadAsStringAsync() |> Async.AwaitTask
+    if not <| System.String.IsNullOrEmpty(token) then
+      return Some token
+    else
+      return None
+  else
+    return None
+  }
+
+let releaseLicenseToken ipAddress token = async {
+  use client = new HttpClient()
+  let! response = 
+    client.GetAsync(sprintf "http://%s:%d/ReleaseLicense/%s" ipAddress port token) 
+    |> Async.AwaitTask
+  if response.IsSuccessStatusCode then
+    printfn "successfully released token."
+  else
+    printfn "failed to release token."
+  }
+
+[<EntryPoint>]
+let main argv =
+  if argv.Length <> 1 then
+    printfn "Please provide the ip-address of the license server as first argument!"
+    System.Environment.Exit(0)
+
+  let ipAddress = argv.[0]
+  let licenseToken = requestLicenseToken ipAddress |> Async.RunSynchronously
+
+  if licenseToken.IsSome then
+    try
+      printfn "Running..."
+      System.Console.Read() |> ignore
+    finally
+      releaseLicenseToken ipAddress licenseToken.Value |> Async.RunSynchronously
+  else
+    printfn "Got no license."
+    System.Console.Read() |> ignore
+
+  0 // return an integer exit code
---
+++ DotNetCrypto/Server/Program.fs
@@ -0,0 +1,99 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open Suave
+open Suave.Http
+open Suave.Web
+open Suave.Operators
+open System.IO
+open System.Xml
+open FSharp.Data
+open Suave.Successful
+open Suave.Filters
+    
+let port = 8083
+
+type private Commands = 
+  | RequestLicense of AsyncReplyChannel<string option>
+  | ReleaseLicense of string * AsyncReplyChannel<bool>
+  | GetLicenseUsage of AsyncReplyChannel<int * int>
+
+let loadLicensesFromXML () =
+  let doc = XmlDocument()
+  doc.Load @"LicenseConfig.xml"
+  doc.SelectSingleNode("//nrOfLicenses").InnerXml |> System.Int32.Parse
+
+[<EntryPoint>]
+let main argv =
+  let licenseManager = MailboxProcessor.Start(fun inbox ->
+    let generateToken () = System.Guid.NewGuid().ToString()
+    let rec loop (freeLicenseCount, usedTokens) = async {
+      let! msg = inbox.Receive()
+
+      match msg with
+      | RequestLicense(reply) when freeLicenseCount > 0 ->
+        let token = generateToken()
+        reply.Reply (Some token)
+        return! loop (freeLicenseCount - 1, usedTokens |> Set.add token )
+      | RequestLicense(reply) ->
+        reply.Reply None
+        return! loop (freeLicenseCount,usedTokens)
+      | ReleaseLicense(token, reply) ->
+        let newSet = usedTokens |> Set.remove token
+        if newSet.Count < usedTokens.Count then
+          reply.Reply true
+          return! loop (freeLicenseCount + 1, newSet)
+        else
+          reply.Reply false
+          return! loop (freeLicenseCount, usedTokens)
+      | GetLicenseUsage(reply) ->
+        reply.Reply(usedTokens.Count, usedTokens.Count + freeLicenseCount)
+        return! loop (freeLicenseCount, usedTokens)
+    }
+    let availableLicenses = loadLicensesFromXML()
+    loop (availableLicenses, Set.empty)
+  )
+  
+  let serverConfig =
+    { defaultConfig with
+        bindings = [ HttpBinding.mkSimple HTTP "0.0.0.0" port
+                     // TODO insert HTTPS binding here
+                   ]
+    }
+
+  ///  we can still use the old symbol but now has a new meaning
+  let foo : WebPart = fun ctx -> GET ctx >>= OK "hello"
+
+  let app : WebPart =
+    choose [
+      GET >=> choose [
+        path "/" >=> OK "Hello"
+        path "/GetLicense" >=> (fun ctx -> async {
+          let! license = licenseManager.PostAndAsyncReply RequestLicense
+          if license.IsSome then
+            return! ctx |> OK license.Value
+          else
+            return! ctx |> RequestErrors.FORBIDDEN "No free license."
+        })
+        pathScan "/ReleaseLicense/%s" (fun token ->
+          fun ctx -> async {
+            let! result = licenseManager.PostAndAsyncReply (fun reply -> ReleaseLicense(token, reply))
+            if result then
+              return! ctx |> OK "Released."
+            else
+              return! ctx |> OK "Unknown token."
+          }
+        )
+        path "/Licenses" >=> (fun ctx -> async {
+          let! used,total = licenseManager.PostAndAsyncReply GetLicenseUsage
+          return! ctx |> (OK (sprintf "%d of %d licenses used." used total))
+        })
+      ]
+    ]
+
+  startWebServer serverConfig app
+
+//  startWebServer serverConfig app
+
+  printfn "%A" argv
+  0 // return an integer exit code
