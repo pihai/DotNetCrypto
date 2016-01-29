4976cdd9923838975d6f926f974dc8305d9ee81a
pihai <philipp-haider@aon.at>
2016-01-29 11:53:56 +0100

    - Changed comments

--- DotNetCrypto/Playground/DataProtectionAPI.fsx
+++ DotNetCrypto/Playground/DataProtectionAPI.fsx
@@ -4,9 +4,9 @@
 open Common
 open System.Security.Cryptography
 
-// The optional entropy is comparable with an initialization vector to add some randomness
-let cipher = ProtectedData.Protect(plainBytes, Array.empty<byte>, DataProtectionScope.CurrentUser)
-let decryptedBytes = ProtectedData.Unprotect(cipher, Array.empty<byte>, DataProtectionScope.CurrentUser)
+let cipher = ProtectedData.Protect(plainBytes, null, DataProtectionScope.CurrentUser)
+let decryptedBytes = ProtectedData.Unprotect(cipher, null, DataProtectionScope.CurrentUser)
 let decryptedTest = decode decryptedBytes
 
+// The optional entropy is comparable with an initialization vector to add some randomness
 // one of the problems of DPAPI is that i relies on the stength of the users password
